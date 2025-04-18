961dcb05dccee092f955991ce2a27812d3916704
Mohammed Parvez <mohammedparvez7457@gmail.com>
2025-04-18 23:33:31 +0530

    Ignore build folder

--- src/navbardata/navbar.js
+++ src/navbardata/navbar.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { Link } from 'react-router-dom';
 import { FaShoppingCart, FaSun, FaMoon } from 'react-icons/fa';
 import CardData from './card.json';
@@ -30,41 +30,30 @@ function Navbar({ setFilteredProducts, cartCount }) {
   }, [searchQuery, setFilteredProducts]);
 
   return (
-    <nav className={`navbar navbar-expand-lg navbar-${theme}  p-3`}>
-      <Link className="navbar-brand text-white" to="/">
-        <img src="/btlogo.png" alt="BUYTIME Logo" width="60" height="60" className="d-inline-block align-top" />
-        
-      </Link>
-      
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-        <span className="navbar-toggler-icon"></span>
-      </button>
+    <nav className={`navbar navbar-${theme}`}>
+  <Link className="navbar-brand" to="/">
+    <img src="/btlogo.png" alt="BUYTIME Logo" width="50" height="50" />
+  </Link>
 
-      <div className="collapse navbar-collapse justify-content-between" id="navbarNav">
-        
-        <form className="form-inline mx-auto d-flex">
-          <input
-            className="form-control"
-            type="search"
-            placeholder="Search for Product"
-            aria-label="Search"
-            value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
-          />
-        </form>
+  <input
+    className="form-control mx-2"
+    type="search"
+    placeholder="Search for Product"
+    value={searchQuery}
+    onChange={(e) => setSearchQuery(e.target.value)}
+  />
 
-        
-        <Link className="btn btn-light square-pill  position-relative" to="/mycart">
-          My Cart <FaShoppingCart />
-          {cartCount > 0 && <span className="cart-badge">{cartCount}</span>}
-        </Link>
+  <Link className="btn btn-light position-relative mx-2" to="/mycart">
+    Cart <FaShoppingCart />
+    {cartCount > 0 && <span className="cart-badge">{cartCount}</span>}
+  </Link>
 
-        
-        <span onClick={toggleTheme} className="ms-4 text-white ">
-          {theme === 'light' ? <FaMoon /> : <FaSun />}
-        </span>
-      </div>
-    </nav>
+  <span onClick={toggleTheme} className="ms-4">
+    {theme === 'light' ? <FaMoon /> : <FaSun />}
+  </span>
+</nav>
+
+  
   );
 }
 
